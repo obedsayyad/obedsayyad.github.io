e48a1dd38015902fb863e12af94c5f5c24fa317f
Obed Sayyad <44410418+obedsayyad@users.noreply.github.com>
2024-01-08 02:12:40 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -50,7 +50,7 @@ function Home() {
         <div className="flex w-full flex-col items-start gap-x-8 gap-y-8 bg-[#131315] px-12 py-10 rounded-3xl max-mdd:max-w-none max-md:p-8">
           <h3 className="max-md:text-[32px] max-md:leading-10 max-md:tracking-[-0.01em]">
             I am a passionate full-stack developer with a love for frontend and graphic
-            design.{" "}
+            design 🦄{" "}
             <span className="text-[#8a8a93]">
               {" "}
               join me to explore my unique projects and creative solutions.
