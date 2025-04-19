d6f109eff4881425720237af7a6bc0c53ed6387e
obedsayyad <obedsayyad123@gmail.com>
2025-04-19 20:47:13 +0000

    Refactor redirect functionality: rename component and move to app directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-// pages/index.tsx
+// app/page.tsx
+"use client";
 import { useEffect } from "react";
 
-export default function Page() {
+export default function Home() {
   useEffect(() => {
     window.location.href = "https://obed1s.vercel.app/";
   }, []);
